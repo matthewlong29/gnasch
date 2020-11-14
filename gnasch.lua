7918067a81b2878a6f8c356ebaf825a5d6eab7c6
Matthew Long <long.matthew29@gmail.com>
2020-11-14 13:41:31 -0500

    update

--- gnasch.lua
+++ gnasch.lua
@@ -1,8 +1,26 @@
+Sandbox = { }
+
+function Sandbox:HelloWorld() 
+    message("Hello World!")
+end
+
+function Sandbox:HideGryphons()
+    MainMenuBarLeftEndCap:Hide()
+    MainMenuBarRightEndCap:Hide()
+end
+
+-- Sandbox:HelloWorld()
+-- Sandbox:HideGryphons()
+
 function gnasch_OnLoad()
-    SlashCmdList["gnasch"] = gnasch_SlashCommand;
-    SLASH_MYADDON1= "/gnasch";
+    print("Gnasch loaded :)")    
+    SLASH_TEST1= "/gnasch";
+    SlashCmdList["TEST"] = gnasch_SlashCommand;
+    message('hello gnasch')
     this:RegisterEvent("VARIABLES_LOADED")
 end
+
+
 function gnasch_SlashCommand()
     print("Hello, Gnasch! :)")
 end
